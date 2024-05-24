6177a3e7ac0f5b7a4ba8b5c84f4c6ff064f6ae40
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-05-23 19:04:49 -0700

    old combination is end of may 23 thursday class

--- old-combination.cs
+++ old-combination.cs
@@ -4,8 +4,7 @@ using UnityEngine.UI;
 public class combination : MonoBehaviour
 {
     public GameObject combinedElementPrefab;
-    public static string combinedString;
-    public ButtonActivation buttonActivation;
+    private UIManager uiManager;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -13,61 +12,75 @@ public class combination : MonoBehaviour
 
         if (otherElement != null)
         {
-            combinedString = CombineElements(otherElement);
+            gameManager.Instance.productTag = CombineElements(otherElement);
         } else
         {
-            combinedString = "";
+            gameManager.Instance.productTag = "NO RETURN TAG PROVIDED";
         }
+        
+        Debug.Log(gameManager.Instance.productTag);
+        uiManager.EnableButtonByTag(gameManager.Instance.productTag);
 
         if (collision.gameObject.name == "delete")
         {
             Destroy(this.gameObject);
         }
+
     }
 
     public string CombineElements(combination otherElement)
     {
         // Check if the combination is valid based on tags
         if ((this.CompareTag("water") && otherElement.CompareTag("air")) ||
-            (this.CompareTag("fire") && otherElement.CompareTag("earth")) ||
-            (this.CompareTag("water") && otherElement.CompareTag("fire")) ||
-            (this.CompareTag("earth") && otherElement.CompareTag("water")) ||
-            (this.CompareTag("lava") && otherElement.CompareTag("water")) ||
-            (this.CompareTag("mud") && otherElement.CompareTag("fire")) ||
-            (this.CompareTag("rock") && otherElement.CompareTag("air")) ||
-            (this.CompareTag("sand") && otherElement.CompareTag("water")) ||
-            (this.CompareTag("air") && otherElement.CompareTag("fire")) ||
-            (this.CompareTag("water vapor") && otherElement.CompareTag("air")) ||
-            (this.CompareTag("smoke") && otherElement.CompareTag("cloud")) ||
-            (this.CompareTag("clay") && otherElement.CompareTag("fire")) ||
-            (this.CompareTag("thunder cloud") && otherElement.CompareTag("air")) ||
-            (this.CompareTag("electric") && otherElement.CompareTag("water vapor")) ||
-            (this.CompareTag("brick") && otherElement.CompareTag("rock")) ||
-            (this.CompareTag("steam generator") && otherElement.CompareTag("electric")))
+         (this.CompareTag("fire") && otherElement.CompareTag("earth")) ||
+         (this.CompareTag("water") && otherElement.CompareTag("fire")) ||
+         (this.CompareTag("fire") && otherElement.CompareTag("water")) ||
+         (this.CompareTag("lava") && otherElement.CompareTag("water")) ||
+         (this.CompareTag("earth") && otherElement.CompareTag("water")) ||
+         (this.CompareTag("mud") && otherElement.CompareTag("fire")) ||
+         (this.CompareTag("rock") && otherElement.CompareTag("air")) ||
+         (this.CompareTag("sand") && otherElement.CompareTag("water")) ||
+         (this.CompareTag("air") && otherElement.CompareTag("fire")) ||
+         (this.CompareTag("water vapor") && otherElement.CompareTag("air")) ||
+         (this.CompareTag("smoke") && otherElement.CompareTag("cloud")) ||
+         (this.CompareTag("clay") && otherElement.CompareTag("fire")) ||
+         (this.CompareTag("thunder cloud") && otherElement.CompareTag("air")) ||
+         (this.CompareTag("electric") && otherElement.CompareTag("water vapor")) ||
+         (this.CompareTag("brick") && otherElement.CompareTag("rock")) ||
+         (this.CompareTag("steam generator") && otherElement.CompareTag("electric")))
         {
- 
+
+        
+
             // Destroy the combining elements
             Destroy(otherElement.gameObject);
-
             Destroy(this.gameObject);
 
-            //what element
-            Debug.Log("The element " + combinedElementPrefab.tag + " has been created");
-
             // Spawn the combined element
-               //combinedString = combinedElementPrefab.tag;
-            gameManager.Instance.productTag = combinedElementPrefab.tag;
-            Debug.Log(gameManager.Instance.productTag + " from combination");
             Instantiate(combinedElementPrefab, transform.position, Quaternion.identity);
+            
+            
+            
+            Debug.Log("debug of combined element prefab : " + combinedElementPrefab.tag);
+      
+            return combinedElementPrefab.tag;
 
-        //    ButtonActivation.activationParent();
-
-            return combinedString;
 
         }
         else
         {
-            return "";
+            return "EMPTY STRING";
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
