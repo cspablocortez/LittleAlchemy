5efe146bf549e551f40786064b3ed512bd71932c
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-05-30 19:05:06 -0700

    TODO roadmap

--- CollisionScript.cs
+++ CollisionScript.cs
@@ -17,7 +17,14 @@ public class CollisionScript : MonoBehaviour
         if (gameObject.tag == "foo" && collision.gameObject.tag == "bar" ||
             gameObject.tag == "bar" && collision.gameObject.tag == "foo")
         {
-          uiManager.EnableButtonByTag("foobar");
+          // 1.
+
+          string combinationTag = ResultingElementTag(gameObject.tag, collision.gameObject.tag);
+          uiManager.EnableButtonByTag(combinationTag);
         }
+    }
+
+    public string ResultingElementTag(string tag1, string tag2) {
+        // 2. Output the tag of the resulting object according to the table on Google Docs
     }
 }
